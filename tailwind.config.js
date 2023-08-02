fd484e2bde9ec9f00dfa3a7191a734db85b20bad
Ankur Rawat <ankurrawat620@gmail.com>
2023-08-02 10:34:13 +0530

    Create the Nav Bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        mullish: ["Mullish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76"
+      }
+    },
   },
   plugins: [],
 }
